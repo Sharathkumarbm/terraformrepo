71c15a1c3652e4adbe35b303ce3efba73df384e8
Sharathkumarbm <157864809+Sharathkumarbm@users.noreply.github.com>
2024-04-27 01:26:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,38 +1,18 @@
-resource "local_file" "num1" {
-  filename = var.v6.c
-  content  = var.v2
+variable "v2" {}
+variable "v3" {}
+variable "v4" {}
+variable "v5" {
+  type = list
 }
-
-resource "local_file" "num2" {
-  filename = var.v3
-  content  = var.v4
+variable "v6" {
+  type = map
 }
-resource "local_file" "num3" {
-  filename = var.v5[0]
-  content  = var.v5[1]
+variable "v7" {
+  type = list
 }
-
-resource "local_file" "num4" {
-  filename = var.v6.a
-  content  = var.v6.b
+variable "v8" {
+  type = list
 }
-
-resource "local_file" "num5" {
-  filename = var.v6.c
-  content  = var.v6.d
-}
-
-resource "local_file" "num6" {
-  filename = var.v7[1][1]
-  content  = var.v7[0][1]
-}
-
-resource "local_file" "num7" {
-  filename = var.v8[0]
-  content  = var.v8[4][0]
-}
-
-resource "local_file" "res8" {
-  filename = var.v9.id
-  content  = var.v9.address[0]
+variable "v9" {
+  type = map
 }

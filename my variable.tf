674721d8fda5a469499c9fdea13dc9988fe0cbad
Sharathkumarbm <157864809+Sharathkumarbm@users.noreply.github.com>
2024-04-27 01:25:57 +0530

    Update my variable.tf

--- my variable.tf
+++ my variable.tf
@@ -1,14 +1,16 @@
-terraform {
-  required_providers {
-    local = {
-      source = "hashicorp/local"
-      version = "2.5.1"
-    }
-  }
+v2 = "content_value_for_num1"
+v3 = "filename_for_num2"
+v4 = "content_value_for_num2"
+v5 = ["filename_for_num3", "content_value_for_num3"]
+v6 = {
+  a = "filename_value_for_num4"
+  b = "content_value_for_num4"
+  c = "filename_and_content_value_for_num1_and_num5"
+  d = "content_value_for_num5"
 }
-
-provider "local" {
-  alias = "second_local"
-  # other configuration settings
+v7 = [["filename_and_content_value_for_num6"], ["content_value_for_num6"]]
+v8 = ["filename_for_num7", ["content_value_for_num7"]]
+v9 = {
+  id = "filename_for_res8"
+  address = ["content_value_for_res8"]
 }
-
